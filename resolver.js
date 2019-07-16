2cddfaf2df161a19449ed3fad83e2c31208ad436
codjoero <rownee2001@gmail.com>
2019-07-16 15:01:02 +0300

    ch(server): query and mutation
    - add type mutation
    - add resolver functions for queries and mutations
    
    [Starts #167315699]

--- resolver.js
+++ resolver.js
@@ -1,5 +1,23 @@
 exports.resolvers = {
   Query: {
-    getAllRecipes: () => {}
+    getAllRecipes: async (root, args, { Recipe } ) => {
+      return await Recipe.find();
+    }
+  },
+  Mutation: {
+    addRecipe: async(
+      root,
+      { name, description, category, instructions, username },
+      { Recipe }
+    ) => {
+      const newRecipe = await new Recipe({
+        name,
+        description,
+        category,
+        instructions,
+        username 
+      }).save();
+      return newRecipe;
+    }
   }
 };
